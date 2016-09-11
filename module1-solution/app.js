d9288105d403f8caff2a1ee1086c14c3004a02b4
shmulikHeifetz <shmulik.hz@gmail.com>
2016-09-11 19:27:35 +0300

    module-1-commit2

---
+++ module1-solution/app.js
@@ -0,0 +1,27 @@
+(function() {
+  'use strict';
+
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope) {
+    $scope.foodItems = "";
+    $scope.message = "";
+    $scope.checkFood = function () {
+      var cleanItems = $scope.foodItems.trim();
+      var message = "Please enter data first"
+
+      if(cleanItems != ""){
+        var itemsArr = cleanItems.split(',');
+        message = itemsArr.length > 3 ? "Too much!" : "Enjoy!";
+      }
+
+      $scope.message = message;
+    }
+
+  }
+
+
+}());
